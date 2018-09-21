d60871c83f29b5a066bb6c5192b7cdcda17a3eef
Joses W. Ho <joseshowh@gmail.com>
2018-09-21 13:59:56 +0800

    add data

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' LSAT Scores.
+#'
+#' Describe the experiment and the dataset.
+#'
+#' @format A list with two elements: \code{pretest} and \code{posttest}.
+#'
+#' @source This was obtained from xxxyyy
+"lsat_scores"
+
+#' Transcription Scores.
+#'
+#' Describe the experiment and the dataset.
+#'
+#' @format A list with two elements: \code{pretest} and \code{posttest}.
+#'
+#' @source This was obtained from xxxyyy
+"transcription_scores"
+
+#' Wellbeing Scores (2 independent groups).
+#'
+#' Describe the experiment and the dataset.
+#'
+#' @format A list with two elements: \code{pretest} and \code{posttest}.
+#'
+#' @source This was obtained from xxxyyy
+"wellbeing_ind"
+
+#' Wellbeing Scores (Before and after design).
+#'
+#' Describe the experiment and the dataset.
+#'
+#' @format A list with two elements: \code{pretest} and \code{posttest}.
+#'
+#' @source This was obtained from xxxyyy
+"wellbeing_paired"
