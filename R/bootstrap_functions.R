d624e7dac156ed1677dacd5a8934b4bf14ae3792
Joses W. Ho <joseshowh@gmail.com>
2018-09-21 11:19:29 +0800

    fix docstring for `bootdiff()`

--- R/bootstrap_functions.R
+++ R/bootstrap_functions.R
@@ -10,10 +10,10 @@ bootdiff <- function(data, control, test, paired, ci = 0.95, reps = 5000,
   #' @param control A column or item in `data`. This is the control group.
   #' @param test A column or item in `data`. This is the test group.
   #' @param paired boolean. If TRUE, the two groups are treated as paired
-  #' samples. The \code{control} group is treated as \code{pret.intervention}
-  #' and the \code{control} group is considered \code{post.intervention}.
+  #' samples. The \code{control} group is treated as pre-intervention
+  #' and the \code{control} group is considered post-intervention.
   #' @param ci float, default 0.95. The level of the confidence intervals
-  #' produced. The default (0.95) produces 95% CIs.
+  #' produced. The default \code{ci = 0.95} produces 95 percent CIs.
   #' @param reps integer, default 5000. The number of bootstrap resamples that
   #' will be generated.
   #' @param func function, default mean. This function will be applied to
@@ -21,27 +21,44 @@ bootdiff <- function(data, control, test, paired, ci = 0.95, reps = 5000,
   #' saved as a single bootstrap resample.
   #'
   #' @return A list with the following 8 elements:
-  #' @param func The \code{func} passed to \code{bootdiff}.
-  #' @param difference The difference between the two groups; effectively
-  #' \code{func(test) - func(control)}.
-  #' @param ci The \code{ci} passed to the \code{bootdiff}.
-  #' @param bca_ci_low The lower limit of the Bias Corrected and Accelerated
-  #' bootstrap confidence interval.
-  #' @param bca_ci_high The upper limit of the Bias Corrected and Accelerated
-  #' bootstrap confidence interval.
-  #' @param pct_ci_low The lower limit of the percentile bootstrap
-  #' confidence interval.
-  #' @param pct_ci_low The upper limit of the percentile bootstrap
-  #' confidence interval.
-  #' @param bootstraps An array of all the bootstraps resamples.
+  #'
+  #' \item{func}{
+  #' The \code{func} passed to \code{bootdiff}.
+  #' }
+  #' \item{difference}{
+  #' The difference between the two groups;
+  #' effectively \code{func(test) - func(control)}.
+  #' }
+  #' \item{ci}{
+  #' The \code{ci} passed to the \code{bootdiff}.
+  #' }
+  #' \item{bca_ci_low}{
+  #' The lower limit of the Bias Corrected and Accelerated bootstrap confidence
+  #' interval.}
+  #'
+  #' \item{bca_ci_high}{
+  #'   The upper limit of the Bias Corrected and Accelerated bootstrap confidence
+  #'   interval.}
+  #'
+  #' \item{pct_ci_low}{
+  #'   The upper limit of the percentile bootstrap confidence interval.
+  #'   }
+  #' \item{pct_ci_high}{
+  #'   The lower limit of the percentile bootstrap confidence interval.
+  #'   }
+  #'
+  #' \item{bootstraps}{
+  #'   The array of bootstrap resamples generated.}
   #'
   #' @examples
   #' # Use the preloaded data.
   #'
   #' @section References:
-  #' Various references will be added here.
+  #' 1. What is bootstrap resampling?
+  #' 2. What are BCa confidence intervals?
   #'
   #' @seealso
+  #' \url{https://www.google.com}
   #'
 
   c <- data[[control]]
@@ -65,25 +82,27 @@ bootdiff <- function(data, control, test, paired, ci = 0.95, reps = 5000,
 
   result = list()
 
+  result$func = func
   result$difference = diff
-  result$bootstraps = boot$t
+  result$ci = ci
   result$bca_ci_low = ci$bca[4]
   result$bca_ci_high = ci$bca[5]
   result$pct_ci_low = ci$percent[4]
   result$pct_ci_high = ci$percent[5]
+  result$bootstraps = boot$t
 
   return(result)
 }
 
 
-create_padded_data_frame <- function(x1, x2, name1, name2) {
-  #' DOCSTRING
-  #'
-  max_len = max(length(x1), length(x2))
-  c = c(x1, rep(NA, max_len - length(x1)))
-  t = c(x2, rep(NA, max_len - length(x2)))
-
-  df <- data.frame(list(c, t))
-  colnames(df) <- c(name1, name2)
-  return(df)
-}
+#' create_padded_data_frame <- function(x1, x2, name1, name2) {
+#'   #' DOCSTRING
+#'   #'
+#'   max_len = max(length(x1), length(x2))
+#'   c = c(x1, rep(NA, max_len - length(x1)))
+#'   t = c(x2, rep(NA, max_len - length(x2)))
+#'
+#'   df <- data.frame(list(c, t))
+#'   colnames(df) <- c(name1, name2)
+#'   return(df)
+#' }
