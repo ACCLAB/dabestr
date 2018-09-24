74af78c63afbf57b9b53b941b58b7baf1db0c25f
Joses W. Ho <joseshowh@gmail.com>
2018-09-24 13:15:45 +0800

    add package docstring

---
+++ R/dabestr.R
@@ -0,0 +1,9 @@
+#' dabestr: A package for producing estimation plots.
+#'
+#' The dabestr package provides a function to generate estimation plots
+#' \code{dabest.plot} and a function to construct bootstrap confidence intervals
+#' for two-group differences \code{bootdiff}.
+#'
+#' @docType package
+#' @name dabestr
+NULL
