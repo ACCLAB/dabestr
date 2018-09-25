1dbfaa183b622ff9163d5c9067825644b61025e5
Joses W. Ho <joseshowh@gmail.com>
2018-09-25 17:53:56 +0800

    update docstring for dabest.plot

--- R/plot.R
+++ R/plot.R
@@ -25,7 +25,7 @@
 #' samples. The group first passed to \code{idx} is treated as pre-intervention
 #' while the second group is considered post-intervention.
 #' @param ci float, default 0.95. The level of the confidence intervals
-#' produced. The default \code{ci = 0.95} produces 95 percent CIs.
+#' produced. The default \code{ci = 0.95} produces 95\% CIs.
 #' @param reps integer, default 5000. The number of bootstrap resamples that
 #' will be generated.
 #' @param fun function, default mean. This function will be applied to
@@ -131,3 +131,68 @@ dabest.plot <- function(data, x, y, idx, fun = mean, reps = 5000) {
   }
   return(list(p1, p2))
 }
+
+
+#' @export
+geom_flat_violin <- function(mapping = NULL, data = NULL, stat = "ydensity",
+                             position = "dodge", trim = TRUE, scale = "area",
+                             show.legend = NA, inherit.aes = TRUE, ...) {
+
+  layer(data = data, mapping = mapping, stat = stat,
+        geom = GeomFlatViolin, position = position, show.legend = show.legend,
+        inherit.aes = inherit.aes,
+        params = list(trim = trim,scale = scale, ...)
+  )
+}
+
+
+"%||%" <- function(a, b) {
+  if (!is.null(a)) a else b
+}
+
+
+GeomFlatViolin <-
+  ggplot2::ggproto(
+    "GeomFlatViolin",
+
+    ggplot2::Geom,
+
+    setup_data = function(data, params) {
+      data$width <- data$width %||%
+        params$width %||%
+        (resolution(data$x, FALSE) * 0.9)
+      # ymin, ymax, xmin, and xmax define the bounding rectangle for each group.
+      data %>%
+        dplyr::group_by(group) %>%
+        dplyr::mutate(ymin = min(y),
+                      ymax = max(y),
+                      xmin = x,
+                      xmax = x + width / 2)
+    },
+
+    draw_group = function(data, panel_scales, coord) {
+      # Find the points for the line to go all the way around
+      data <- transform(data, xminv = x,
+                        xmaxv = x + violinwidth * (xmax - x))
+
+      # Make sure it's sorted properly to draw the outline
+      newdata <- rbind(plyr::arrange(transform(data, x = xminv), y),
+                       plyr::arrange(transform(data, x = xmaxv), -y))
+
+      # Close the polygon: set first and last point the same.
+      # Needed for coord_polar and such.
+      newdata <- rbind(newdata, newdata[1,])
+
+      ggplot2:::ggname("geom_flat_violin",
+                       GeomPolygon$draw_panel(newdata,
+                                              panel_scales, coord))
+    },
+
+    draw_key = ggplot2::draw_key_polygon,
+
+    default_aes = ggplot2::aes(weight = 1, colour = "grey20",
+                               fill = "grey", size = 0.5, alpha = NA,
+                               linetype = "solid"),
+
+    required_aes = c("x", "y")
+  )
