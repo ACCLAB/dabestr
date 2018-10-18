d776173b6aba0003fb2435aaf972360dc3896b73
Joses W. Ho <joseshowh@gmail.com>
2018-10-18 14:01:16 +0800

    add `get_tick_positions()`

--- R/plot_helpers.R
+++ R/plot_helpers.R
@@ -15,6 +15,22 @@ get_tick_labels <- function(plot.obj, axes) {
 
 
 
+get_tick_positions <- function(plot.obj, axes) {
+  # This works for ggplot2 v3.0.0; not fully tested with other versions yet.
+  plot.obj.build <- ggplot2::ggplot_build(plot.obj)
+
+  if (axes == "x") {
+    pos <- plot.obj.build$layout$panel_params[[1]]$y.minor_source
+  } else if (axes == "y") {
+    pos <- plot.obj.build$layout$panel_params[[1]]$y.major_source
+  } else {
+    stop('`axes` must be either "x" or "y"')
+  }
+
+  return(pos)
+}
+
+
 
 max_nchar_ticks <- function(tick_list) {
   tick_nchars <- sapply(tick_list, nchar)
