45920c3a0a71b9ea23f65e891fc77820694fd34b
Joses W. Ho <joseshowh@gmail.com>
2020-04-20 14:51:39 +0800

    update docs

--- R/main.R
+++ R/main.R
@@ -24,7 +24,7 @@
 #' @param x,y Columns in \code{.data}.
 #'
 #' @param idx Accepts a vector containing factors or strings in the \code{x}
-#'   columns, or a list containing vectors as noted above.
+#'   column, or a list containing vectors as noted above.
 #'   If a vector is supplied, the first element will be the control group,
 #'   so all differences will be computed for every other group and this first group.
 #'   If a list of vectors is supplied, a multi-plot will be generated,
