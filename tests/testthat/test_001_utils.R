e01fb35df12d63eec6309581d5a318c9d75aee59
Rosa Castillo <arosacastillo@protonmail.com>
2024-04-04 18:05:16 +0200

    renaming tests and adding test_utils

--- tests/testthat/test_001_utils.R
+++ tests/testthat/test_001_utils.R
@@ -1,4 +1,8 @@
 #### Detecting non-valid dabest objects
 testthat::test_that("Able to detect non-valid dabest objects", {
-  expect_error(dabestr::check_dabest_object(32), "dabest_obj must be a <dabest> obj")
+  expect_error(check_dabest_object(32), "dabest_obj must be a <dabest> obj")
+})
+
+testthat::test_that("Able to detect non-valid dabest objects", {
+  expect_error(check_effectsize_object("any_object"), "dabest_effectsize_obj must be a <dabest_effectsize> obj")
 })
