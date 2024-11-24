2e1adc7ea79ce31c4d5bf5230497a756c291f7e9
sunroofgod <e0725238@u.nus.edu>
2024-11-25 02:12:19 +0800

    fix: fixing namespace issue for .data

--- R/002_plot_utils.R
+++ R/002_plot_utils.R
@@ -383,13 +383,15 @@ initialize_raw_plot <- function(plot_kwargs, plot_components, dabest_effectsize_
   }
   return(list(raw_plot, raw_y_range, raw_y_min, x_axis_raw))
 }
-
+#'
 #' Adds Swarm Bars to a Raw Data Plot
 #'
 #' This function takes a `dabest_effectsize_obj` and enhances its raw data plot by adding swarm bars.
 #' It utilizes the provided plotting parameters along with specific x and y values to customize the
 #' appearance and positioning of the swarm bars. The `y_min` parameter ensures that the swarm bars
 #' are appropriately placed within the plot's y-axis limits.
+#' 
+#' @importFrom rlang .data
 #'
 #' @param dabest_effectsize_obj A `dabest_effectsize_obj` created by the [effect_size()] function.
 #' @param plot_kwargs A list of parameters used to adjust and control the appearance of the plot.
@@ -453,12 +455,14 @@ add_swarm_bars_to_raw_plot <- function(dabest_effectsize_obj, plot_kwargs, x_val
     show.legend = FALSE
   ))
 }
-
+#'
 #' Adds Contrast Bars to a Delta Plot
 #'
 #' This function takes a `dabest_effectsize_obj` and augments its delta plot by adding contrast bars.
 #' It utilizes the provided plotting parameters and specific x and y values to customize the appearance
 #' of the contrast bars based on the main violin plot type.
+#' 
+#' @importFrom rlang .data
 #'
 #' @param dabest_effectsize_obj A `dabest_effectsize_obj` created by the [effect_size()] function.
 #' @param plot_kwargs A list of parameters used to adjust and control the appearance of the plot.
@@ -533,12 +537,14 @@ add_contrast_bars_to_delta_plot <- function(dabest_effectsize_obj, plot_kwargs, 
     alpha = alpha
   ))
 }
-
+#'
 #' Adds Delta Text to a Delta Plot
 #'
 #' This function enhances a delta plot by adding delta text using the provided parameters.
 #' It takes a delta plot, a dabest_effectsize_obj, plotting parameters, and specific x and y values.
 #' The function also considers the main violin type and float contrast to customize the delta text.
+#' 
+#' @importFrom rlang .data
 #'
 #' @param delta_plot A ggplot object representing the delta plot.
 #' @param dabest_effectsize_obj A dabest_effectsize_obj created by the effect_size() function.
@@ -728,6 +734,8 @@ create_delta_dots_data <- function(dabest_effectsize_obj, x_axis_breaks) {
 #' This function enhances a delta plot by adding delta dots using the provided parameters.
 #' It takes a delta plot, a dabest_effectsize_obj, plotting parameters, x-axis breaks,
 #' and delta dots data. The function also considers the main violin type to customize the delta dots.
+#' 
+#' @importFrom rlang .data
 #'
 #' @param delta_plot A ggplot object representing the delta plot.
 #' @param dabest_effectsize_obj A dabest_effectsize_obj created by the effect_size() function.
